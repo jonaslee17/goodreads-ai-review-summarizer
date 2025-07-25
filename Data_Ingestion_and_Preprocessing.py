ed5db69f2edbe4d16906c73dbccdadb156d86983
jonaslee17 <uhz7mq@virginia.edu>
2025-07-24 22:58:53 -0400

    update file

--- Data_Ingestion_and_Preprocessing.py
+++ Data_Ingestion_and_Preprocessing.py
@@ -1,11 +1,13 @@
 import sqlite3
 import pandas as pd
 
+## Loading datasets from SQLite and CSV files
+
 # Connect to the book details database
 book_details_df = pd.read_csv('/Users/jonaslee/Downloads/archive/Book_Details.csv')
 book_details_df.head()
 
-#connect to reviews database
+# Connect to reviews database
 conn_reviews = sqlite3.connect('/Users/jonaslee/Downloads/archive/book_reviews.db')
 pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", conn_reviews)
 
@@ -13,7 +15,7 @@ pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", conn_rev
 reviews_df = pd.read_sql_query("SELECT * FROM book_reviews", conn_reviews)
 reviews_df.head()
 
-#connect to books database
+# Connect to books database
 conn_books = sqlite3.connect('/Users/jonaslee/Downloads/archive/books.db')
 pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", conn_books)
 
@@ -21,17 +23,20 @@ pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", conn_boo
 books_df = pd.read_sql_query("SELECT * FROM books", conn_books)
 books_df.head()
 
+## Cleaning the data
+
 # Drop reviews without actual text
 reviews_df = reviews_df.dropna(subset=['review_content'])
-# rids of leading/trailing whitespace in review_content
+# Rid of leading/trailing whitespace in review_content
 reviews_df['review_content'] = reviews_df['review_content'].str.strip()
 
-# drop rows where review content is very short (spam?)
+# Drop rows where review content is very short (spam?)
 reviews_df = reviews_df[reviews_df['review_content'].str.len() > 30]
 
-# drop rows without review_rating
+# Drop rows without review_rating
 reviews_df = reviews_df[reviews_df['review_rating'].notna()]
 
-# Preview cleaned reviews
+# Preview first 10 cleaned reviews
 print(reviews_df[['review_content', 'review_rating']].head(10))
 
+## Grouping reviews by book
