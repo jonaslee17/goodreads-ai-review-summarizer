7b32d84e2d10d28c6a8dbe24ac53794509107f7e
jonaslee17 <uhz7mq@virginia.edu>
2025-08-03 16:13:28 -0400

    first 100 tester; added progress bar

--- Review_Summarizer.py
+++ Review_Summarizer.py
@@ -1,16 +1,28 @@
 import pandas as pd
 from transformers import pipeline
+from tqdm import tqdm
 
 books_with_reviews = pd.read_csv('/Users/jonaslee/Desktop/Goodreads_AI_Review_Summarizer/reviews_grouped_per_book.csv')
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+# Testing first 100
+books_with_reviews = books_with_reviews.head(100)
+
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
 def summarize_reviews(review_list):
     combined = " ".join(review_list)
     # Bart has 1024 token limit
     return summarizer(combined[:1024])[0]['summary_text']
 
+# Progress bar
+summaries = []
+for reviews in tqdm(books_with_reviews['review_content'].apply(eval), desc="Summarizing Reviews"):
+    summary = summarize_reviews(reviews)
+    summaries.append(summary)
+
 # Apply summarizer
-books_with_reviews['summary'] = books_with_reviews['review_content'].apply(eval).apply(summarize_reviews)
+books_with_reviews['summary'] = summaries
 
 # New csv
-books_with_reviews.to_csv("books_with_summaries.csv", index=False)
+books_with_reviews.to_csv("books_with_summaries_first_100.csv", index=False)
+print("Summarization complete. Check 'books_with_summaries_first_100.csv' for results.")
